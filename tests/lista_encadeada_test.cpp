df15a702b502b95c9e54a40138da9e4da7ae4306
Kaio Felps <kaiofelipe2005@gmail.com>
2025-04-14 12:38:00 -0300

    test: add initiial unit test for `ListaEncadeada<T>`

--- tests/lista_encadeada_test.cpp
+++ tests/lista_encadeada_test.cpp
@@ -1,15 +1,15 @@
 #include <catch2/catch_all.hpp> 
 #include "lista_encadeada.hpp"
+#include <iostream>
 
 TEST_CASE("It should find the correct position of a node by its value", "[ListaEncadeada::getpos]") {
     auto* lista = new Listas::ListaEncadeada<int>();
-
+    
     lista->pushback(20);
     lista->pushback(30);
     lista->pushback(2);
     lista->pushback(493);
     lista->pushback(770);
-
 
     REQUIRE(lista->getpos(20) == 0);
     REQUIRE(lista->getpos(30) == 1);
