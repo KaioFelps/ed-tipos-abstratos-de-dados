ce0d3d6817a2f33ee9893908cbb75acd6bdfead8
Kaio Felps <kaiofelipe2005@gmail.com>
2025-04-14 12:19:20 -0300

    fix(lista_encadeada.hpp): add #pragma once directive

---
+++ include/lista_encadeada.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <memory>
+#include <optional>
+#include <concepts>
+
+namespace Listas
+{
+
+template<typename T>
+requires std::equality_comparable<T>
+class NóListaEncadeada
+{
+    private:
+        T element_;
+        T* previous_;
+        T* next_;
+
+    public:
+        NóListaEncadeada(T element);
+        ~NóListaEncadeada();
+
+        T& element();
+        T takeelement();
+        bool ishead();
+        bool hasnext();
+
+        NóListaEncadeada<T>* getnext();
+        NóListaEncadeada<T>* getprevious();
+
+        void setnext(NóListaEncadeada<T>* element);
+        void setprevious(NóListaEncadeada<T>* element);
+};
+
+template<typename T>
+requires std::equality_comparable<T>
+class ListaEncadeada
+{
+    private:
+        NóListaEncadeada<T>* head_;
+        NóListaEncadeada<T>* _tail();
+
+    public:
+        ListaEncadeada();
+        ~ListaEncadeada();
+
+        T& pushfront(T element);
+        T& pushback(T element);
+        T& insert(T element, size_t offset);
+
+        T& tail();
+        T& head();
+        T* get(size_t index);
+        size_t getpos(const T& element) const;
+
+        std::optional<T> popfront();
+        std::optional<T> poplast();
+        std::optional<T> remove();
+
+        size_t size() const;
+        bool isempty() const;
+
+        void print() const;
+};
+
+}
