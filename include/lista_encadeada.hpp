70434b359ab03d9adac85da86ad8d6fbde9ee75b
Kaio Felps <kaiofelipe2005@gmail.com>
2025-04-14 23:56:26 -0300

    change(ListaEncadeada): add docstrings to every method and change `int` values which indicates position to `size_t` type

--- include/lista_encadeada.hpp
+++ include/lista_encadeada.hpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <optional>
 #include <concepts>
+#include "listas_utils.hpp"
 
 namespace Listas
 {
@@ -44,20 +45,62 @@ class ListaEncadeada
         ListaEncadeada();
         ~ListaEncadeada();
 
+        /// @brief Adiciona um elemento no início da lista.
+        /// @param element 
+        /// @return Uma referência do elemento recém adicionado.
         T& pushfront(T element);
+
+        /// @brief Adiciona um elemento no final da lista.
+        /// @param element 
+        /// @return Uma referência do elemento recém adicionado.
         T& pushback(T element);
+
+        /// @brief Adiciona o `element` logo após o `offset`ésimo elemento. 
+        /// @param element 
+        /// @param offset 
+        /// @return Uma referência do elemento recém adicionado.
+        /// @throw `std::out_of_range` se `offset` >= tamanho da lista.
         T& insert(T element, size_t offset);
 
+        /// @brief Obtém o último item da lista.
+        /// @return Uma referência para o último item da lista.
+        /// @throw `std::out_of_range` se a lista estiver vazia.
         T& tail();
+
+        /// @brief Obtém o primeiro item da lista.
+        /// @return Uma referência para o primeiro item da lista.
+        /// @throw `std::out_of_range` se a lista estiver vazia.
         T& head();
+
+        /// @brief Obtém um ponteiro para o item na posição `index`.
+        /// @param index 
+        /// @return Um ponteiro (que pode ser nulo) do elemento na posição `index`.
         T* get(size_t index);
+
+        /// @brief Obtém o índice (posição) da primeira ocorrência de `element` dentro da lista.
+        /// @param element 
+        /// @return Um `size_t` representando a posição da primeira ocorrência do `element` na lista.
+        /// Caso `element` não esteja na lista, é retornado `-1`.
         size_t getpos(const T& element) const;
 
+        /// @brief Remove o primeiro elemento da lista.
+        /// @return O elemento removido, se a lista não estiver vazia. Caso contrário, um `nullopt` é retornado.
         std::optional<T> popfront();
+        
+        /// @brief Remove o último elemento da lista.
+        /// @return O elemento removido, se a lista não estiver vazia. Caso contrário, um `nullopt` é retornado.
         std::optional<T> poplast();
-        std::optional<T> remove();
 
+        /// @brief Remove o elemento na posição `index` da lista.
+        /// @return O elemento removido. Se a posição for inválida, um `nullopt` é retornado.
+        std::optional<T> remove(size_t index);
+
+        /// @brief Obtém o tamanho da lista.
+        /// @return Um `size_t` contendo a quantidade de items na lista.
         size_t size() const;
+
+        /// @brief Verifica se a lista está vazia.
+        /// @return `true` se e somente se houver ao menos 1 elemento na lista. `false` caso contrário.
         bool isempty() const;
 
         void print() const;
